fed6d23638c5b3a58aecf429b89403bf1f1ea391
Daniel <satelerd@gmail.com>
2021-08-31 15:32:31 -0400

    added ¿how to use?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import './App.css';
 // import Logo from './smart-dimension.png'
 import Card from './components/card/Card.js'
 
+const cardTitle = [
+  <h3 className="card-title">Paso 1</h3>,
+  <h3 className="card-title">Paso 2</h3>,
+  <h3 className="card-title">Paso 3</h3>
+]
+
+const cardBody = [
+  <div>
+    <h3 className="card-body">
+      Busca un modelo que cumpla con los siguientes requisitos:
+      <li>Debe ser un archivo .stl</li>
+      <li>No puede pesar mas de x[Gb]</li>
+    </h3>
+    <h5>
+      Puedes buscar el modelo en nuestro catalogo o de alguna otra fuente (recomendamos <a href="https://www.thingiverse.com/" target="_blank" rel="noreferrer">thingiverse</a> para buscar el modelo y <a href="https://3dviewer.net/" target="_blank" rel="noreferrer">3dviewer</a> para visualizarlo)
+    </h5>
+  </div>,
+  <h3 className="card-body">Elije el tamaño, material y calidad (con ayuda de nuestro visualizador)</h3>,
+  <h3 className="card-body">¡Listo! Smart Dimension te conectara con el impresor mas cercano y llegara a tu casa dentro de los proximos 2 dias habiles</h3>
+]
+
+
 function App() {
   return (
     <div className="App">
@@ -19,26 +41,26 @@ function App() {
             <h3>Imprime en 3D de manera facil</h3>
           </div>
           <div className="logo">
-            <img src="https://static.thenounproject.com/png/358773-200.png" alt=""/>
+            <img src="https://static.thenounproject.com/png/358773-200.png" alt="" />
           </div>
-          
+
         </div>
 
 
         <div className="howto">
           <h4>¿Como usar?</h4>
           <div className="upper-card">
-            <Card className="paso1" classN="upper-card"></Card>
+            <Card className="paso1" title={cardTitle[0]} body={cardBody[0]}></Card>
           </div>
           <div className="bottom">
             <div className="bottom-card">
-              <Card className="paso2" classN="bottom-card"></Card>
+              <Card className="paso2" title={cardTitle[1]} body={cardBody[1]}></Card>
             </div>
             <div className="bottom-card">
-              <Card className="paso3" classN="bottom-card"></Card>
+              <Card className="paso3" title={cardTitle[2]} body={cardBody[2]}></Card>
             </div>
           </div>
-          
+
 
         </div>
 
@@ -46,6 +68,9 @@ function App() {
           <button>Ya tengo mi diseño</button>
         </div>
 
+        <div className="footer">
+          <h6><a href="https://github.com/satelerd" target="_blank" rel="noreferrer">Creado por SAT</a></h6>
+        </div>
 
       </div>
 
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,14 @@
+import React from 'react'
 import './Card.css';
 
-function Card(classN) {
-    return(
+function Card({ title, body }) {
+    return (
         <div className="card">
             <div className="title">
-                <h3 className="card-title">Primer paso</h3>
+                {title}
             </div>
             <div className="card-body">
-                <h3 className="card-body">Busca un modelo que cumpla con los requisitos</h3>
+                {body}
             </div>
         </div>
     );
